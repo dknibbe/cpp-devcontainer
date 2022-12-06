f926adbdd1aa7c57e775216e8a261a7cdfa2f637
Daniel Knibbe <daniel.knibbe@rocketmail.com>
2022-12-06 06:32:20 +0000

    Tests and precision

--- temperature/TemperatureConverter.cpp
+++ temperature/TemperatureConverter.cpp
@@ -3,11 +3,11 @@
 namespace TemperatureConverter {
 double celsius2fahrenheit(double temperature) {
   // °C * 1,8 + 32
-  return temperature * 1.8 + 32.0;
+  return temperature * 9.0 / 5.0 + 32.0;
 }
 
 double fahrenheit2celsius(double temperature) {
   // (°F - 32) * 5/9
-  return (temperature - 32) * 5.0 / 9.0;
+  return (temperature - 32.0) * 5.0 / 9.0;
 }
 } // namespace TemperatureConverter
--- temperature/TemperatureConverterTest.cpp
+++ temperature/TemperatureConverterTest.cpp
@@ -1,18 +1,33 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include <TemperatureConverter.hpp>
 
 TEST_CASE("0 degrees celsius") {
-  REQUIRE(TemperatureConverter::celsius2fahrenheit(0.0) == 32.0);
+  REQUIRE_THAT(TemperatureConverter::celsius2fahrenheit(0.0),
+               Catch::Matchers::WithinRel(32.0));
 }
 
 TEST_CASE("100 degrees celsius") {
-  REQUIRE(TemperatureConverter::celsius2fahrenheit(100.0) == 212.0);
+  REQUIRE_THAT(TemperatureConverter::celsius2fahrenheit(100.0),
+               Catch::Matchers::WithinRel(212.0));
+}
+
+TEST_CASE("0 degrees fahrenheit") {
+  REQUIRE_THAT(TemperatureConverter::fahrenheit2celsius(0.0),
+               Catch::Matchers::WithinRel(-17.7, 1e-2));
+}
+
+TEST_CASE("100 degrees fahrenheit") {
+  REQUIRE_THAT(TemperatureConverter::fahrenheit2celsius(100.0),
+               Catch::Matchers::WithinRel(37.7, 1e-2));
 }
 
 TEST_CASE("Roundtrips") {
-  REQUIRE(TemperatureConverter::celsius2fahrenheit(
-              TemperatureConverter::fahrenheit2celsius(0.0)) == 0.0);
-  REQUIRE(TemperatureConverter::fahrenheit2celsius(
-              TemperatureConverter::celsius2fahrenheit(0.0)) == 0.0);
+  REQUIRE_THAT(TemperatureConverter::celsius2fahrenheit(
+                   TemperatureConverter::fahrenheit2celsius(0.0)),
+               Catch::Matchers::WithinRel(0.0));
+  REQUIRE_THAT(TemperatureConverter::fahrenheit2celsius(
+                   TemperatureConverter::celsius2fahrenheit(0.0)),
+               Catch::Matchers::WithinRel(0.0));
 }
